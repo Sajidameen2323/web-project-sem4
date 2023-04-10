a70066f80c5b34acca6b6b4e7eaacd07e619b3a6
lucifer2323 <sajidameen063@gmail.com>
2023-04-10 23:35:55 +0530

    User Seeding

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['Admin', 'Manager', 'Employee'])->default('Employee');
             $table->rememberToken();
             $table->timestamps();
         });
