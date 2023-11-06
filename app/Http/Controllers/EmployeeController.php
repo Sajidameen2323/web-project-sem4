570a103591352ea7d10e186b6a374e1011ae83c3
Sajid Ameen <42244729+Sajidameen2323@users.noreply.github.com>
2023-11-06 20:17:03 +0530

    update pro pic links

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -28,7 +28,7 @@ class EmployeeController extends Controller
                 $emp->role = $role->role;
                 $profilePicUrl = ProfilePic::where('user_id', $emp->id)->first();
                 if ($profilePicUrl) {
-                    $emp->pro_pic = asset('storage/' . $profilePicUrl->pic);
+                    $emp->pro_pic = asset('/' . $profilePicUrl->pic);
                 } else {
                     $emp->pro_pic = "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ";
                 }
